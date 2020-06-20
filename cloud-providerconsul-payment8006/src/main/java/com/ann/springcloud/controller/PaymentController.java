22f2ffd30edbadff2a86ce0fa2702ba03591f638
longquan <longquanann@163.com>
2020-06-20 09:51:53 +0800

    + add consul demo

---
+++ cloud-providerconsul-payment8006/src/main/java/com/ann/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.ann.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author longquan
+ */
+@Slf4j
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/consul")
+    public String paymentConsul() {
+        return "spring cloud with consul：" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+}
