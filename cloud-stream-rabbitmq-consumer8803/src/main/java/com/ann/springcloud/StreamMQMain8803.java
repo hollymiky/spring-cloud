5cfbdbe90398e89f26e28cdd8f9fe446f3412100
longquan <longquanann@163.com>
2020-06-24 11:31:37 +0800

    + add spring cloud stream demo

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/ann/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.ann.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author longquan
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
